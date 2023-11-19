d1a533b48916fc8ca059fa8ee0c24a4935254cdc
dell <mouhamed.dore@etud.univ-jfc.fr>
2023-11-19 14:59:27 +0100

    tp11

--- src/main/java/bowling/Tour.java
+++ src/main/java/bowling/Tour.java
@@ -3,9 +3,9 @@ package bowling;
 public class Tour {
 
 	private int numTour;
-	private int nbQuilleTombeesLancer1 = 0;
-	private int nbQuilleTombeesLancer2 = 0;
-	private int nbQuilleTombeesLancer3 = 0;
+	private int nbQuTmbLancer1 = 0;
+	private int nbQuTmbLancer2 = 0;
+	private int nbQuTmbLancer3 = 0;
 	private int numCoup = 1;
 	boolean estFini = false;
 
@@ -18,48 +18,48 @@ public class Tour {
 	}
 
 	public boolean estStrike(){
-		return nbQuilleTombeesLancer1 == PartieMonoJoueur.Nbre_Quilles;
+		return nbQuTmbLancer1 == PartieMonoJoueur.Nbre_Quilles;
 	}
 
 	public boolean estSpare(){
-		return numCoup == 2 && nbQuilleTombeesLancer1+nbQuilleTombeesLancer2 == PartieMonoJoueur.Nbre_Quilles && numTour != PartieMonoJoueur.Nbre_Tours;
+		return numCoup == 2 && nbQuTmbLancer1 + nbQuTmbLancer2 == PartieMonoJoueur.Nbre_Quilles && numTour != PartieMonoJoueur.Nbre_Tours;
 	}
 
 	public void lancer(int valeur){
 		switch (numCoup){
 			case 1:
-				nbQuilleTombeesLancer1 += valeur;
-				if (nbQuilleTombeesLancer1 == PartieMonoJoueur.Nbre_Quilles && numTour != PartieMonoJoueur.Nbre_Tours) estFini = true;
-				else if (nbQuilleTombeesLancer1 == PartieMonoJoueur.Nbre_Quilles) numCoup += 1;
+				nbQuTmbLancer1 += valeur;
+				if (nbQuTmbLancer1 == PartieMonoJoueur.Nbre_Quilles && numTour != PartieMonoJoueur.Nbre_Tours) estFini = true;
+				else if (nbQuTmbLancer1 == PartieMonoJoueur.Nbre_Quilles) numCoup += 1;
 				else numCoup++;
 				break;
 			case 2:
-				nbQuilleTombeesLancer2 += valeur;
+				nbQuTmbLancer2 += valeur;
 				estFini = true;
 				if (numTour == PartieMonoJoueur.Nbre_Tours) {
-					if (!(nbQuilleTombeesLancer1+nbQuilleTombeesLancer2 < PartieMonoJoueur.Nbre_Quilles)){
+					if (!(nbQuTmbLancer1 + nbQuTmbLancer2 < PartieMonoJoueur.Nbre_Quilles)){
 						numCoup++;
 						estFini = false;
 					}
 				}
 				break;
 			case 3:
-				nbQuilleTombeesLancer3 += valeur;
+				nbQuTmbLancer3 += valeur;
 				estFini = true;
 				break;
 		}
 	}
 
 	public int getNbQuilleTombeesLancer1() {
-		return nbQuilleTombeesLancer1;
+		return nbQuTmbLancer1;
 	}
 
 	public int getNbQuilleTombeesLancer2() {
-		return nbQuilleTombeesLancer2;
+		return nbQuTmbLancer2;
 	}
 
 	public int getNbQuilleTombeesLancer3() {
-		return nbQuilleTombeesLancer3;
+		return nbQuTmbLancer3;
 	}
 
 	public int getNumCoup() {
